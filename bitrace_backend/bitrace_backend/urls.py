35621467cc9b1897213bace11a31128f21e0119e
Dongho Yu <n0rr7882@gmail.com>
2020-03-01 00:37:08 +0900

    feat: JWT auth

--- bitrace_backend/bitrace_backend/urls.py
+++ bitrace_backend/bitrace_backend/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 builtin_urls = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('auth/session/', include('rest_framework.urls')),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 local_app_urls = [
