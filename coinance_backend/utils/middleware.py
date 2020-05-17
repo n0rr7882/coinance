d40030933dd4fe89de48ed42c1c34f073afa91cd
Dongho Yu <n0rr7882@gmail.com>
2020-05-17 22:47:32 +0900

    Disable csrf validation

--- coinance_backend/utils/middleware.py
+++ coinance_backend/utils/middleware.py
@@ -1,3 +1,6 @@
-class DisableCSRFMiddleware(object):
+from django.utils.deprecation import MiddlewareMixin
+
+
+class DisableCSRFMiddleware(MiddlewareMixin):
     def process_request(self, request):
         setattr(request, '_dont_enforce_csrf_checks', True)
