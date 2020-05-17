697d105baf0a6c9e903a07a708bfedcf9a905745
Dongho Yu <n0rr7882@gmail.com>
2020-05-17 21:42:52 +0900

    Add cors origin whitelists

--- coinance_backend/coinance_backend/settings/production.py
+++ coinance_backend/coinance_backend/settings/production.py
@@ -4,4 +4,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://api.coinance.norrz.io",
+    "http://api.coinance.norrz.io",
+    "https://coinance.norrz.io",
+    "http://coinance.norrz.io",
+]
