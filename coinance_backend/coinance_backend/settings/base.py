249844a60ff6cace97278fe93257ee9fe7977ed0
Dongho Yu <n0rr7882@gmail.com>
2020-05-17 22:44:41 +0900

    Disable csrf validation

--- coinance_backend/coinance_backend/settings/base.py
+++ coinance_backend/coinance_backend/settings/base.py
@@ -94,6 +94,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'utils.middleware.DisableCSRFMiddleware',
 ]
 
 ROOT_URLCONF = 'coinance_backend.urls'
