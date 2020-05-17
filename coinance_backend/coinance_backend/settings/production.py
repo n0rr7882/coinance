79ad8708c97700495ab2d2a8f053f8f855c057d7
Dongho Yu <n0rr7882@gmail.com>
2020-05-17 18:34:22 +0900

    Allow all cors origin

--- coinance_backend/coinance_backend/settings/production.py
+++ coinance_backend/coinance_backend/settings/production.py
@@ -2,8 +2,4 @@ from .base import *  # pylint: disable=unused-wildcard-import
 
 DEBUG = False
 
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "https://coinance.norrz.io",
-]
+CORS_ORIGIN_ALLOW_ALL = True
