18d10b339ed60dac6ec5a3617b7c431b02d2dae8
Dongho Yu <n0rr7882@gmail.com>
2020-06-21 22:18:40 +0900

    Change wallet chart library

--- coinance-frontend/src/components/WalletChart/index.tsx
+++ coinance-frontend/src/components/WalletChart/index.tsx
@@ -1,41 +1,66 @@
 import React from "react";
-import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
+import { Doughnut } from "react-chartjs-2";
 import { Wallet } from "../../models/wallet";
+import { useTheme, makeStyles } from "@material-ui/core";
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+const useWalletChartStyles = makeStyles({
+  container: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 interface Props {
   wallets: Wallet[];
 }
 
 const WalletChart: React.FC<Props> = ({ wallets }) => {
+  const classes = useWalletChartStyles();
+  const theme = useTheme();
+  const COLOR_SET = [
+    theme.palette.primary.dark,
+    theme.palette.primary.main,
+    theme.palette.primary.light,
+    theme.palette.secondary.dark,
+    theme.palette.secondary.main,
+    theme.palette.secondary.light,
+    theme.palette.info.dark,
+    theme.palette.info.main,
+    theme.palette.info.light,
+    theme.palette.success.dark,
+    theme.palette.success.main,
+    theme.palette.success.light,
+    theme.palette.warning.dark,
+    theme.palette.warning.main,
+    theme.palette.warning.light,
+    theme.palette.error.dark,
+    theme.palette.error.main,
+    theme.palette.error.light,
+  ];
+
   const composed = wallets.map((wallet) => ({
     id: wallet.id,
     name: wallet.currency.symbol,
     value: wallet.aggregated_amount_to_start_currency_price,
   }));
 
+  const data = {
+    labels: composed.map((d) => d.name),
+    datasets: [
+      {
+        data: composed.map((d) => d.value),
+        backgroundColor: COLOR_SET,
+      },
+    ],
+  };
+
   return (
-    <div style={{ height: 360 }}>
-      <ResponsiveContainer>
-        <PieChart>
-          <Pie
-            data={composed}
-            innerRadius={80}
-            outerRadius={120}
-            paddingAngle={3}
-            fill="#8884d8"
-            dataKey="value"
-            label={({ name, percent }) =>
-              `${name}(${(Number(percent || 0) * 100).toFixed(0)}%)`
-            }
-          >
-            {composed.map((wallet, i) => (
-              <Cell key={wallet.id} fill={COLORS[i % COLORS.length]} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
+    <div className={classes.container}>
+      <Doughnut data={data} />
     </div>
   );
 };
